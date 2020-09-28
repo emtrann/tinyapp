eb047efbf4e23d1523a1c7cf947e6c2b6695b3ab
Michelle Tran <michelletraan@hotmail.com>
2020-09-28 23:40:26 +0000

    Add web form + sending form data to backend

--- express_server.js
+++ express_server.js
@@ -4,10 +4,22 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs")
 
+function generateRandomString() {
+  let availableChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let shortURLString = ''; 
+  for (let i = 0; i < 6; i++) {
+    shortURLString += availableChars[Math.floor(Math.random() * 62)];
+  }
+  return shortURLString;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,9 +38,18 @@ app.get("/urls", (req, res) => {
   res.render("url_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");         
 });
 
 app.listen(PORT, () => {
